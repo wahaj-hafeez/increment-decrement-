1d4cf2b2d07bc7924563b22d8b1c25dacde81a7d
wahaj <wahaj.hafeez@codedistrict.com>
2022-02-17 16:19:54 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import React,{ useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [number,setNumber]=useState(0);
   return (
     <div className={styles.container}>
       <Head>
@@ -20,36 +22,28 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
+     <div style={{display : "flex" , flexDirection : "inline",marginInline:"10"}}>
+     <button onclick="activateLasers()" style={{ backgroundColor: "black",
+  color: "white",
+  fontSize: "20px",
+  padding: "10px 40px",
+  borderRadius: 50,
+  margin: "30px 0px",
+  cursor: "pointer"}}>
+  Increment
+</button>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+       <h1 style={{margin : 10,fontSize : 70}}>{number}</h1>
+       <button onclick="activateLasers()" style={{ backgroundColor: "black",
+  color: "white",
+  fontSize: "20px",
+  padding: "10px 40px",
+  borderRadius: 50,
+  margin: "30px 0px",
+  cursor: "pointer"}}>
+  Decreament
+</button>
+     </div>
       </main>
 
       <footer className={styles.footer}>
