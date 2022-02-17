93d0f45602d4a16773bf4aa88908144789c278d9
wahaj <wahaj.hafeez@codedistrict.com>
2022-02-17 17:12:08 +0500

    change color to green

--- components/incdec.js
+++ components/incdec.js
@@ -6,7 +6,7 @@ export default function Incdec(){
   const [number,setNumber]=useState(0);
     return (
         <div style={{display : "flex" , flexDirection : "inline",marginInline:"10"}}>
-     <button onClick={()=>{setNumber(number+1)}} style={{ backgroundColor: "black",
+     <button onClick={()=>{setNumber(number+1)}} style={{ backgroundColor: "green",
   color: "white",
   fontSize: "20px",
   padding: "10px 40px",
@@ -17,7 +17,7 @@ export default function Incdec(){
 </button>
 
        <h1 style={{margin : 10,fontSize : 70}}>{number}</h1>
-       <button onClick={()=>{setNumber(number-1)}} style={{ backgroundColor: "black",
+       <button onClick={()=>{setNumber(number-1)}} style={{ backgroundColor: "green",
   color: "white",
   fontSize: "20px",
   padding: "10px 40px",
